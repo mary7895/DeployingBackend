aa75e5cc5bb881b3cc2e5f620fbb599f146d09d9
finalproject17 <finallproject17@gmail.com>
2024-06-12 10:06:21 +0300

    get method

--- controllers/appliedJobsController.js
+++ controllers/appliedJobsController.js
@@ -1,24 +1,31 @@
 const AppliedJob = require('../models/appliedJobsModel');
 
-
 exports.applyForJob = async (req, res) => {
+  const { jobId, userId } = req.body;
+
   try {
-    const { jobId, userId } = req.body;
+    const existingAppliedJob = await AppliedJob.findOne({ jobId, userId });
+    if (existingAppliedJob) {
+      return res.status(400).json({ message: 'This Job is already applied for by this user!!!!' });
+    }
+
     const appliedJob = new AppliedJob({ jobId, userId });
     await appliedJob.save();
+
     res.status(201).json({ message: 'You applied successfully ^_^ GOOD LUCK' });
   } catch (error) {
+    console.error(error);
     res.status(500).json({ error: error.message });
   }
 };
 
-
 exports.getAllAppliedJobs = async (req, res) => {
   try {
     const jobId = req.params.jobId;
-    const AllAppliedJobs = await AppliedJob.find({ jobId }).populate('userId');
-    res.json(AllAppliedJobs);
+    const allAppliedJobs = await AppliedJob.find({ jobId }).populate('userId');
+    res.json(allAppliedJobs);
   } catch (error) {
+    console.error(error);
     res.status(500).json({ error: error.message });
   }
 };
@@ -29,6 +36,7 @@ exports.deleteAppliedJob = async (req, res) => {
     await AppliedJob.findByIdAndDelete(applicationId);
     res.json({ message: 'This Applied Job deleted successfully' });
   } catch (error) {
+    console.error(error);
     res.status(500).json({ error: error.message });
   }
 };
