03e42ffc65434283dba350e0976e0a6c271c7cc2
finalproject17 <finallproject17@gmail.com>
2024-06-21 21:44:14 +0300

    Update appliedJobsController.js

--- controllers/appliedJobsController.js
+++ controllers/appliedJobsController.js
@@ -22,14 +22,28 @@ exports.applyForJob = async (req, res) => {
 exports.getAllAppliedJobs = async (req, res) => {
   try {
     const jobId = req.params.jobId;
-    const allAppliedJobs = await AppliedJob.find({ jobId }).populate('userId');
-    res.json(allAppliedJobs);
+    const { page = 1, limit = 8 } = req.query;
+
+    const limitInt = parseInt(limit, 10);
+    const pageInt = parseInt(page, 10);
+
+    const AllAppliedJobs = await AppliedJob.find({ jobId })
+      .limit(limitInt)
+      .skip((pageInt - 1) * limitInt)
+      .populate('userId');
+
+    const totalItems = await AppliedJob.countDocuments({ jobId });
+
+    res.json({
+      totalItems,
+      totalPages: Math.ceil(totalItems / limitInt),
+      currentPage: pageInt,
+      data: AllAppliedJobs
+    });
   } catch (error) {
-    console.error(error);
     res.status(500).json({ error: error.message });
   }
 };
-
 exports.deleteAppliedJob = async (req, res) => {
   try {
     const applicationId = req.params.applicationId;
