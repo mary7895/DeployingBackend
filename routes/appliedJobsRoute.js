c8bc2efc55a8bb5e1b6a5e60be93d0a873127246
finalproject17 <finallproject17@gmail.com>
2024-06-26 04:35:42 +0300

    Update appliedJobsRoute.js

--- routes/appliedJobsRoute.js
+++ routes/appliedJobsRoute.js
@@ -4,6 +4,7 @@ const appliedJobController = require('../controllers/appliedJobsController');
 
 router.post('/', appliedJobController.applyForJob);
 router.get('/:jobId', appliedJobController.getAllAppliedJobs);
+router.get('/get/:userId', appliedJobController.getAllAppliedJobsByJobSeeker);
 router.delete('/:applicationId', appliedJobController.deleteAppliedJob);
 
 
